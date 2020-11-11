5816c9a7e1ceed619422836a9088fe1884b085a8
ChinwenduSilvia <silviachinwendu@gmail.com>
2020-11-11 07:17:33 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, { useState } from 'react'
-import data from "./data";
-import List from "./list";
+import React from 'react'
 
 const App = () => {
-  const [people, setPeople] = useState(data);
-
   return (
-    <section className="min-vh-100 d-flex align-items-center bg-grey p-md-5">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-6 text-center mb-5 mb-lg-5 bg-white rounded-lg shadow-lg p-md-5">
-            <h2> Birthday celebration today ({people.length})</h2>
-            <List people={people}/>
-            <div>
-              <button className="btn btn-md btn-grey text-white mb-md-0 mb-3" onClick={() => setPeople([])}>Remove all</button>
-            </div>
-          </div>
-
-        </div>
-        
-
-      </div>
+    <div>
       
-    </section>
+    </div>
   )
 }
 
-export default App;
+export default App
