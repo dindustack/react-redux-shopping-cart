994bcb47aa21777c6a4c64d15b15405d226eb12c
ChinwenduSilvia <silviachinwendu@gmail.com>
2020-11-13 14:02:42 +0100

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Loading from "./Loading";
+import Tours from "./Tours";
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);  
+  const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  }
+
+  const fetchTours = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <section className="py-5">
+            <div className="container justify-content-center text-center py-5">
+            <h1 className="text-success mb-3">Successfully deleted all tours</h1>
+            <button className="btn btn-outline-success font-weight-bold" onClick={()=> fetchTours()}>Refresh travel guide</button>
+            </div>
+        </section>
+    )
+  }
   return (
     <div>
-      
+      <Tours tours={tours} removeTour={removeTour}/>
     </div>
   )
 }
---
+++ src/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Loading = () => {
+    return (
+        <section className="py-5">
+            <div className="container justify-content-center text-center py-5">
+            <h1 className="text-success">loading...</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Loading
