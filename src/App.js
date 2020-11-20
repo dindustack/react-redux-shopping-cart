0cef58f974f1f9e2f5d7ac17632f3fc208ba353b
ChinwenduSilvia <silviachinwendu@gmail.com>
2020-11-20 11:43:57 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import React, { useState } from "react";
-import items from "./Data";
-import Categories from "./Categories";
-import Menu from "./Menu";
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+import React from 'react'
+
 const App = () => {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
-
   return (
-    <section className="min-vh-100 d-flex align-items-center pt-5 pt-md-5">
-      <div className="container">
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </div>
-    </section>
-  );
-};
+    <div>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
